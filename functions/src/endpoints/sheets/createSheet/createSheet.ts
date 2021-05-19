9f5cafd04857c9121858abbd72e08b85c469f243
Hikaru <hikaru.outsmart@gmail.com>
2021-05-18 23:40:46 -0300

    added some error threatment for createSheet endpoint

--- functions/src/endpoints/sheets/createSheet/createSheet.ts
+++ functions/src/endpoints/sheets/createSheet/createSheet.ts
@@ -1,20 +1,28 @@
-import {Request, Response} from 'express'
-import { CallOfCthulhuInvestigator } from '../../../models/characterSheets/CallOfCthulhuInvestigator.js'
-import { getUserFromToken } from '../../../utils/getUserFromToken/getUserFromToken.js'
-import { firestore } from 'firebase-admin'
+import { Request, Response } from "express";
+import { CallOfCthulhuInvestigator } from "../../../models/characterSheets/CallOfCthulhuInvestigator.js";
+import { getUserFromToken } from "../../../utils/getUserFromToken/getUserFromToken.js";
+import { firestore } from "firebase-admin";
 
 export const createSheetEndpoint = async (req: Request, res: Response) => {
-  const auth = req.headers.token as string
-  const user = await getUserFromToken(auth)
-  if(!user) {
-    res.status(401)
-    res.send('User not found, please login')
-    return
+  const auth = req.headers.token as string;
+  const user = await getUserFromToken(auth);
+  if (!user) {
+    res.status(401);
+    res.send("User not found, please login");
+    return;
   }
   const database = firestore();
-  const newSheet = req.body as CallOfCthulhuInvestigator
-  await database.collection('COCSheets').doc(newSheet.id).set(newSheet);
-  user.COCInvestigatorIds.push({id: newSheet.id, name: newSheet.info.name })
-  await database.collection('users').doc(user.username).set(user);
-  res.send('Character successfully created!')
-}
+  const newSheet = req.body as CallOfCthulhuInvestigator;
+  if (
+    user.COCInvestigatorIds.findIndex((sheet) => sheet.id === newSheet.id) < 0
+  ) {
+    await database.collection("COCSheets").doc(newSheet.id).set(newSheet);
+    user.COCInvestigatorIds.push({ id: newSheet.id, name: newSheet.info.name });
+    await database.collection("users").doc(user.username).set(user);
+    res.send("Character successfully created!");
+  } else {
+    //TODO set error code
+    res.send("That character already exists");
+
+  }
+};
